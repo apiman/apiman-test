f2a765414e626928d8c10a3e4cf247599f329a88
Jakub Cechacek <jcechace@redhat.com>
2016-02-22 16:08:09 +0100

    KC docker image can now be use as standalone KC for apiman

--- tools/docker/keycloak/scripts/docker-entrypoint.sh
+++ tools/docker/keycloak/scripts/docker-entrypoint.sh
@@ -2,26 +2,36 @@
 
 
 function wait_for_server() {
-    while ! grep 'WildFly 8.2.0.Final "Tweek" started' ${JBOSS_HOME}/standalone/log/server.log; do
+    while ! grep -e 'Keycloak\s.*\sstarted\s.*in\s[0-9]*ms' ${JBOSS_HOME}/standalone/log/server.log; do
         sleep 5
     done
     echo ">>>>> SERVER STARTED <<<<<"
 }
 
-if [ -z ${REALM_SET+x} ]
+# make sure we get fresh logfile logfile
+rm ${JBOSS_HOME}/standalone/log/server.log 2> /dev/null
+
+# configure and start server
+: ${CONFIGURED_FILE:="${JBOSS_HOME}/configured"}
+
+if [ ! -f $CONFIGURED_FILE ]
 then
+    ${JBOSS_HOME}/bin/add-user.sh -r master -u ${KC_USER_NAME} -p ${KC_USER_PASSWORD}
     ${JBOSS_HOME}/bin/standalone.sh -b 0.0.0.0      \
         -Dkeycloak.migration.action=import          \
-        -Dkeycloak.migration.provider=singleFile    \
-        -Dkeycloak.migration.file=${REALM_FILE}     &
+        -Dkeycloak.migration.provider=dir           \
+        -Dkeycloak.migration.dir=${REALM_DIR}       &
 
     WILDFLY_PID=$!
-    echo "export REALM_SET=true" >> ~/.profile
+    touch ${CONFIGURED_FILE}
 
 else
     ${JBOSS_HOME}/bin/standalone.sh -b 0.0.0.0 &
     WILDFLY_PID=$!
 fi
+
+#wait for server to start
+wait_for_server
 
 trap "kill -TERM ${WILDFLY_PID} && wait ${WILDFLY_PID}" TERM INT
 
