04f37a4bb6d7ed80bceac9f2ce74ee54744788bd
Miroslav Jaros <mjaros@redhat.com>
2016-03-12 22:41:04 +0100

    Add travis ci configuration

--- tools/scripts/maven/build_rest_tests.sh
+++ tools/scripts/maven/build_rest_tests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 PROPERTIES=(
-"-Dapiman.keystore=$APIMAN_KEYSTORE"
-"-Dapiman.home=$APIMAN_HOME"
-"-Dapiman.test.proxy.address.server=$MACHINE_IP"
-)
+    "-Dapiman.keystore=$APIMAN_KEYSTORE"
+    "-Dapiman.home=$APIMAN_HOME"
+    "-Dapiman.test.proxy.address.server=$MACHINE_IP"
+    )
 
 mvn -f ${TEST_SOURCES}/pom.xml clean install -Prest-tests $PROPERTIES
