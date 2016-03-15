760bffc8e8ffc271508ec7dc79efcdc256c9b9ca
Miroslav Jaros <mjaros@redhat.com>
2016-03-15 22:34:32 +0100

    Fix output in travis builds

--- tools/scripts/travis/travis_run.sh
+++ tools/scripts/travis/travis_run.sh
@@ -4,4 +4,4 @@ DIR=$(dirname ${BASH_SOURCE[0]})
 
 source $DIR/installrc.sh
 
-bash -x $DIR/../tests_runner.sh > $WORKSPACE/out.log 2>&1 || (cat $WORKSPACE/out.log && false)
+bash $DIR/../tests_runner.sh > $WORKSPACE/out.log 2>&1 || (cat $WORKSPACE/out.log && false)
