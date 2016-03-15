a719537c5795f7b3faecc4ef82f6944e534a29cf
Ondrej Pontes <opontes@op.brq.redhat.com>
2016-03-15 09:54:51 +0100

    Fix TransformationPolicyIT XML to JSON

---
+++ apiman-it-commons/src/main/java/io/apiman/test/integration/base/entity/TestDataRoot.java
@@ -0,0 +1,19 @@
+package io.apiman.test.integration.base.entity;
+
+/**
+ * @author opontes
+ */
+public class TestDataRoot {
+    private TestData testData;
+
+    public TestDataRoot() {
+    }
+
+    public TestData getTestData() {
+        return testData;
+    }
+
+    public void setTestData(TestData testData) {
+        this.testData = testData;
+    }
+}
