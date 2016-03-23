5894eb6fd2942f1d6412da9559d85de2f94e52c6
pstanko <pstanko@redhat.com>
2016-03-23 17:38:36 +0100

    Simple Header UI Tests - 2 simple cases - working

---
+++ apiman-it-ui/src/test/java/io/apiman/test/integration/ui/tests/apis/policies/SimpleHeaderPluginPolicyIT.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2016 Red Hat Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.apiman.test.integration.ui.tests.apis.policies;
+
+import io.apiman.test.integration.base.policies.PolicyDefs;
+import io.apiman.test.integration.runner.annotations.entity.Plugin;
+import io.apiman.test.integration.ui.support.selenide.pages.apis.detail.ApiPoliciesDetailPage;
+import io.apiman.test.integration.ui.support.selenide.pages.policies.AddSimpleHeaderPolicyPage;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by pstanko.
+ * @author pstanko
+ */
+@Plugin(artifactId = "apiman-plugins-simple-header-policy")
+public class SimpleHeaderPluginPolicyIT extends AbstractApiPolicyIT {
+
+    private AddSimpleHeaderPolicyPage addPolicyPage;
+
+    @Before
+    public void openPage(){
+        addPolicyPage = policiesDetailPage.addPolicy(AddSimpleHeaderPolicyPage.class);
+    }
+
+    @Override
+    protected String getDefinitionId() {
+        return PolicyDefs.SIMPLE_HEADER_POLICY;
+    }
+
+    @Test
+    public void shouldAddRequestPolicy(){
+        addPolicyPage
+            .addHeader("X-Request", "Request value",
+                AddSimpleHeaderPolicyPage.ValueType.String, AddSimpleHeaderPolicyPage.ApplyTo.Request, true)
+            .addPolicy(AddSimpleHeaderPolicyPage.class);
+        assertPolicyPresent();
+    }
+
+    @Test
+    public void shouldStripSomePolicy(){
+        addPolicyPage
+            .stripHeader(AddSimpleHeaderPolicyPage.HeaderType.Value, AddSimpleHeaderPolicyPage.WithMatcher.Regex, "ahoj")
+            .addPolicy(AddSimpleHeaderPolicyPage.class);
+        assertPolicyPresent();
+    }
+
+
+
+
+}
