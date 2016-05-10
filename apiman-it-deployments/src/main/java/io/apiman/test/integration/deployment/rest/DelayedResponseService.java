3850a695a8e0decea450691c10ae16e7ddc2c769
EsoRimmer <410313@mail.muni.cz>
2016-05-10 13:08:09 +0200

    Rest test for Circuit Breaker Policy

---
+++ apiman-it-deployments/src/main/java/io/apiman/test/integration/deployment/rest/DelayedResponseService.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2016 Red Hat Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.apiman.test.integration.deployment.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+
+import static io.apiman.test.integration.Suite.waitFor;
+
+/**
+ * Created by jsmolar.
+ */
+@Path("/response")
+public class DelayedResponseService {
+
+    @GET
+    @Path("/delayedResponse/{delay}")
+    public Response delayResponse(@PathParam("delay") int delay, @QueryParam("status") Integer status) {
+        waitFor(delay, "Waiting %d milliseconds for delayed response from DelayedResponseService");
+
+        if(status != null) {
+            return Response.status(status).entity("Delayed response by " + delay
+                    + " milliseconds with status code " + status).build();
+        } else {
+            return Response.ok().entity("Delayed response by " + delay + " milliseconds").build();
+        }
+    }
+}
---
+++ apiman-it-rest/src/test/java/io/apiman/test/integration/rest/plugins/policies/circuitbreakerplugin/CircuitBreakerApiPolicyIT.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2016 Red Hat Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.apiman.test.integration.rest.plugins.policies.circuitbreakerplugin;
+
+import io.apiman.manager.api.beans.apis.ApiVersionBean;
+import io.apiman.test.integration.categories.PluginTest;
+import io.apiman.test.integration.categories.PolicyTest;
+import io.apiman.test.integration.runner.annotations.misc.Endpoint;
+import io.apiman.test.integration.runner.annotations.misc.ManagedEndpoint;
+import io.apiman.test.integration.runner.annotations.misc.Policies;
+import io.apiman.test.integration.runner.annotations.version.ApiVersion;
+import org.junit.experimental.categories.Category;
+
+/**
+ * Created by jsmolar.
+ */
+@Category({PolicyTest.class, PluginTest.class})
+public class CircuitBreakerApiPolicyIT extends AbstractCircuitBreakerPolicyIT {
+
+    @ApiVersion(api = "api", endpoint = @Endpoint(value = ENDPOINT), unique = true,
+            policies = @Policies(value = "plugins/circuit_breaker",
+                    params = {"limit", "1", "reset", "2", "error", RESPONSE_ERROR_CODE, "failure", FAILURE_CODE}))
+    private ApiVersionBean apiVersion;
+
+    @ManagedEndpoint("apiVersion")
+    private String ednpoint;
+
+    @Override
+    protected String getResourceURL() {
+        return ednpoint;
+    }
+
+}
