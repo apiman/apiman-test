01ce1087e2b4d8bba36da69bd3b7926065f84e01
Jakub Cechacek <jcechace@gmail.com>
2017-12-04 17:28:50 +0100

    Support for test ex/inclusion

--- tools/scripts/maven/run_rest_tests.sh
+++ tools/scripts/maven/run_rest_tests.sh
@@ -7,6 +7,12 @@ PROPERTIES=(
     "-Dapiman.keystore=$APIMAN_KEYSTORE"
     "-Dapiman.home=$APIMAN_HOME"
     "-Dapiman.test.proxy.address.server=$MACHINE_IP"
+    "-Dtest.exclude='${EXCLUDE_TESTS}'"
+    "-Dtest.include='${INCLUDE_TESTS:-*}'"
     )
 
-mvn -f ${TEST_SOURCES}/pom.xml clean install -Prest-tests $PROPERTIES -Dtest.exclude="**/ipblacklist/*,**/ipwhitelist/*"
+set +x
+
+mvn -f ${TEST_SOURCES}/pom.xml clean install -Prest-tests ${PROPERTIES} -fae
+
+set -x
